384548cc0fae9c26a1e1256d007f18262bd94aa5
Sundaisy <daisyzs1213@163.com>
2018-07-22 14:32:14 +0800

    add eslint

--- javascript/spec/collection-operation-spec.js
+++ javascript/spec/collection-operation-spec.js
@@ -1,9 +1,9 @@
-const { teachers, courses, scores, students } = require("../data");
-const Utils = require("../utils");
+const { teachers, courses, scores, students } = require('../data');
+const Utils = require('../utils');
 
-describe("collection operation", function() {
+describe('collection operation', function() {
 
-    it("查询student中的所有记录的sname、Ssex和class列", () => {
+    it('查询student中的所有记录的sname、Ssex和class列', () => {
         const expected = [
             { sname: '曾华', ssex: '男', class: 95033 },
             { sname: '匡明', ssex: '男', class: 95031 },
@@ -21,14 +21,14 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询教师所有的单位中不重复的Depart列", () => {
+    it('查询教师所有的单位中不重复的Depart列', () => {
         const expected = ['计算机系', '电子工程系'];
 
         const actual = [...new Set(teachers.map(teacher => teacher.depart))];
         expect(actual).toEqual(expected);
     });
 
-    it("查询Score中成绩在60到80之间的所有记录", () => {
+    it('查询Score中成绩在60到80之间的所有记录', () => {
         const expected = [
             { sno: 105, cno: '3-245', degree: 75 },
             { sno: 109, cno: '3-245', degree: 68 },
@@ -42,7 +42,7 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询Score中成绩为85，86或88的记录", () => {
+    it('查询Score中成绩为85，86或88的记录', () => {
         const expected = [
             { sno: 103, cno: '3-245', degree: 86 },
             { sno: 105, cno: '3-105', degree: 88 },
@@ -53,23 +53,23 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询Student中“95031”班或性别为“女”的同学记录", () => {
+    it('查询Student中“95031”班或性别为“女”的同学记录', () => {
         const expected = [
-            { sno: 109, sname: '王芳', ssex: '女', sbirthday: "1975-02-10", class: 95031 }
+            { sno: 109, sname: '王芳', ssex: '女', sbirthday: '1975-02-10', class: 95031 }
         ];
 
         const actual = students.filter(student => student.class === 95031 && student.ssex === '女');
         expect(actual).toEqual(expected);
     });
 
-    it("以Class降序查询Student的所有记录", () => {
+    it('以Class降序查询Student的所有记录', () => {
         const expected = [
-            { sno: 108, sname: '曾华', ssex: '男', sbirthday: "1999-09-01", class: 95033 },
-            { sno: 107, sname: '王丽', ssex: '女', sbirthday: "1976-01-23", class: 95033 },
-            { sno: 101, sname: '李军', ssex: '男', sbirthday: "1976-02-20", class: 95033 },
-            { sno: 105, sname: '匡明', ssex: '男', sbirthday: "1975-10-02", class: 95031 },
-            { sno: 109, sname: '王芳', ssex: '女', sbirthday: "1975-02-10", class: 95031 },
-            { sno: 103, sname: '陆君', ssex: '男', sbirthday: "1974-06-03", class: 95031 }
+            { sno: 108, sname: '曾华', ssex: '男', sbirthday: '1999-09-01', class: 95033 },
+            { sno: 107, sname: '王丽', ssex: '女', sbirthday: '1976-01-23', class: 95033 },
+            { sno: 101, sname: '李军', ssex: '男', sbirthday: '1976-02-20', class: 95033 },
+            { sno: 105, sname: '匡明', ssex: '男', sbirthday: '1975-10-02', class: 95031 },
+            { sno: 109, sname: '王芳', ssex: '女', sbirthday: '1975-02-10', class: 95031 },
+            { sno: 103, sname: '陆君', ssex: '男', sbirthday: '1974-06-03', class: 95031 }
         ];
 
         const arr = [
@@ -94,7 +94,7 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("以Cno升序、Degree降序查询Score的所有记录", () => {
+    it('以Cno升序、Degree降序查询Score的所有记录', () => {
         const arr = [
             { x: 4, y: 3 },
             { x: 2, y: 1 },
@@ -111,17 +111,17 @@ describe("collection operation", function() {
         console.log('------------');
         const sort2 = sort1.sort((s1, s2) => s2.y - s1.y);
         console.log(sort2);
-        fail("unimplement");
+        fail('unimplement');
     });
 
-    it("查询“95031”班的学生人数", () => {
+    it('查询“95031”班的学生人数', () => {
         const expected = 3;
 
         const actual = students.filter(student => student.class === 95301).length;
         expect(actual).toEqual(actual);
     });
 
-    it("查询Score中的最高分的学生学号和课程号", () => {
+    it('查询Score中的最高分的学生学号和课程号', () => {
         const expected = [{ sno: 103, cno: '3-105' }];
 
         const degrees = scores.map(score => score.degree);
@@ -135,7 +135,7 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询‘3-105’号课程的平均分", () => {
+    it('查询‘3-105’号课程的平均分', () => {
         const expected = 81.5;
 
         const allClass = scores.filter(score => score.cno === '3-105');
@@ -144,8 +144,8 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询Score中至少有5名学生选修的并以3开头的课程的平均分数", () => {
-        const expected = { '3-105': 81.5 }
+    it('查询Score中至少有5名学生选修的并以3开头的课程的平均分数', () => {
+        const expected = { '3-105': 81.5 };
 
         let caculateAverage = (numberArr) => {
             const sumScore = numberArr.reduce((accu, cur) => accu + cur.degree, 0);
@@ -169,11 +169,11 @@ describe("collection operation", function() {
             let average = caculateAverage(filterScore);
             accum[curClass] = average;
             return accum;
-        }, {})
+        }, {});
         expect(actual).toEqual(expected);
     });
 
-    it("查询最低分大于70，最高分小于90的Sno列", () => {
+    it('查询最低分大于70，最高分小于90的Sno列', () => {
         const expected = [105, 108];
 
         const snoObj = scores.reduce((accum, cur) => {
@@ -193,39 +193,39 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询所有学生的Sname、Cno和Degree列", () => {
+    it('查询所有学生的Sname、Cno和Degree列', () => {
         const expected = [
-            { cno: '3-105', degree: 78, sname: '曾华' },
-            { cno: '6-166', degree: 81, sname: '曾华' },
-            { cno: '3-245', degree: 75, sname: '匡明' },
-            { cno: '3-105', degree: 88, sname: '匡明' },
-            { cno: '3-105', degree: 91, sname: '王丽' },
-            { cno: '6-106', degree: 79, sname: '王丽' },
-            { cno: '3-105', degree: 64, sname: '李军' },
-            { cno: '6-166', degree: 85, sname: '李军' },
-            { cno: '3-245', degree: 68, sname: '王芳' },
-            { cno: '3-105', degree: 76, sname: '王芳' },
-            { cno: '3-245', degree: 86, sname: '陆君' },
-            { cno: '3-105', degree: 92, sname: '陆君' }
-        ]
+            {cno: '3-105', degree: 78, sname: '曾华'},
+            {cno: '6-166', degree: 81, sname: '曾华'},
+            {cno: '3-245', degree: 75, sname: '匡明'},
+            {cno: '3-105', degree: 88, sname: '匡明'},
+            {cno: '3-105', degree: 91, sname: '王丽'},
+            {cno: '6-106', degree: 79, sname: '王丽'},
+            {cno: '3-105', degree: 64, sname: '李军'},
+            {cno: '6-166', degree: 85, sname: '李军'},
+            {cno: '3-245', degree: 68, sname: '王芳'},
+            {cno: '3-105', degree: 76, sname: '王芳'},
+            {cno: '3-245', degree: 86, sname: '陆君'},
+            {cno: '3-105', degree: 92, sname: '陆君'}
+        ];
 
         let actual = [];
         students.forEach(student => {
             let newScore = scores.filter(score => {
                 if (score.sno === student.sno) {
                     delete score.sno;
-                    score.sname = student.sname
+                    score.sname = student.sname;
                     return score;
                 }
 
-            })
+            });
             actual.push(...newScore);
-        })
+        });
 
         expect(actual).toEqual(expected);
     });
 
-    it("查询所有学生的Sno、Cname和Degree列", () => {
+    it('查询所有学生的Sno、Cname和Degree列', () => {
         let actual = [];
 
         let queryCourseName = (cno) => {
@@ -234,7 +234,7 @@ describe("collection operation", function() {
                     return course.cname;
                 }
             });
-            console.log(item, 'dfjafj')
+            console.log(item, 'dfjafj');
         };
 
         students.forEach(student => {
@@ -244,47 +244,47 @@ describe("collection operation", function() {
                     return score;
                 }
 
-            })
+            });
             actual.push(...newScore);
-        })
+        });
 
         // console.log(actual)
         expect(actual).toEqual(expected);
     });
 
-    it("查询所有学生的Sname、Cname和Degree列", () => {
-        fail("unimplement");
+    it('查询所有学生的Sname、Cname和Degree列', () => {
+        fail('unimplement');
     });
 
-    it("查询“95033”班所选课程的平均分", () => {
+    it('查询“95033”班所选课程的平均分', () => {
         const expected = 80;
 
         const allStudens = students.filter(student => student.class === 95033);
         let classSumScore = 0;
         let classSumNum = 0;
         allStudens.forEach(student => {
-            let oneStudentClass = scores.filter(score => score.sno === student.sno)
+            let oneStudentClass = scores.filter(score => score.sno === student.sno);
             let oneStudentSumScore = Utils.caculateSum(oneStudentClass, 'degree');
             classSumNum += oneStudentClass.length;
             classSumScore += oneStudentSumScore;
-        })
+        });
 
         const actual = Math.round(classSumScore / classSumNum);
 
         expect(actual).toEqual(expected);
     });
 
-    it("现查询所有同学的Sno、Cno和rank列", () => {
-        fail("unimplement");
+    it('现查询所有同学的Sno、Cno和rank列', () => {
+        fail('unimplement');
     });
 
-    it("查询选修“3-105”课程的成绩高于“109”号同学成绩的所有同学的记录", () => {
+    it('查询选修“3-105”课程的成绩高于“109”号同学成绩的所有同学的记录', () => {
         const expected = [
             { sno: 103, cno: '3-105', degree: 92 },
             { sno: 105, cno: '3-105', degree: 88 },
             { sno: 107, cno: '3-105', degree: 91 },
             { sno: 108, cno: '3-105', degree: 78 }
-        ]
+        ];
 
         let specifyClass = scores.filter(score => score.cno === '3-105');
         let speccifyScore = specifyClass.find(classItem => classItem.sno === 109).degree;
@@ -292,7 +292,7 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    it("查询score中选学一门以上课程的同学中分数为非最高分成绩的记录", () => {
+    it('查询score中选学一门以上课程的同学中分数为非最高分成绩的记录', () => {
         const expected = [
             { sno: 101, cno: '3-105', degree: 64 },
             { sno: 101, cno: '6-166', degree: 85 },
@@ -305,7 +305,7 @@ describe("collection operation", function() {
             { sno: 108, cno: '6-166', degree: 81 },
             { sno: 109, cno: '3-245', degree: 68 },
             { sno: 109, cno: '3-105', degree: 76 }
-        ]
+        ];
 
         let studentMap = Utils.caculateRepeateNum(scores, 'sno');
         let allRecords = [];
@@ -322,7 +322,7 @@ describe("collection operation", function() {
         expect(actual).toEqual(expected);
     });
 
-    fit("查询成绩高于学号为“109”、课程号为“3-105”的成绩的所有记录", () => {
+    fit('查询成绩高于学号为“109”、课程号为“3-105”的成绩的所有记录', () => {
         const expected = [
             { sno: 103, cno: '3-245', degree: 86 },
             { sno: 103, cno: '3-105', degree: 92 },
@@ -332,107 +332,107 @@ describe("collection operation", function() {
             { sno: 101, cno: '6-166', degree: 85 },
             { sno: 107, cno: '6-106', degree: 79 },
             { sno: 108, cno: '6-166', degree: 81 }
-        ]
+        ];
 
         let specifyRecord = scores.find(score => score.sno === 109 && score.cno === '3-105');
         const actual = scores.filter(score => score.degree > specifyRecord.degree);
         expect(actual).toEqual(expected);
     });
 
-    it("查询和学号为108的同学同年出生的所有学生的Sno、Sname和Sbirthday列", () => {
-        fail("unimplement");
+    it('查询和学号为108的同学同年出生的所有学生的Sno、Sname和Sbirthday列', () => {
+        fail('unimplement');
     });
 
-    it("查询“张旭“教师任课的学生成绩", () => {
-        fail("unimplement");
+    it('查询“张旭“教师任课的学生成绩', () => {
+        fail('unimplement');
     });
 
-    it("查询选修某课程的同学人数多于5人的教师姓名", () => {
-        fail("unimplement");
+    it('查询选修某课程的同学人数多于5人的教师姓名', () => {
+        fail('unimplement');
     });
 
-    it("查询95033班和95031班全体学生的记录", () => {
-        fail("unimplement");
+    it('查询95033班和95031班全体学生的记录', () => {
+        fail('unimplement');
     });
 
-    it("查询存在有85分以上成绩的课程Cno.", () => {
-        fail("unimplement");
+    it('查询存在有85分以上成绩的课程Cno.', () => {
+        fail('unimplement');
     });
 
-    it("查询出“计算机系“教师所教课程的成绩", () => {
-        fail("unimplement");
+    it('查询出“计算机系“教师所教课程的成绩', () => {
+        fail('unimplement');
     });
 
-    it("查询“计算机系”与“电子工程系“不同职称的教师的Tname和Prof", () => {
-        fail("unimplement");
+    it('查询“计算机系”与“电子工程系“不同职称的教师的Tname和Prof', () => {
+        fail('unimplement');
     });
 
-    it("查询选修编号为“3-105“课程且成绩至少高于选修编号为“3-245”的同学的Cno、Sno和Degree,并按Degree从高到低次序排序", () => {
-        fail("unimplement");
+    it('查询选修编号为“3-105“课程且成绩至少高于选修编号为“3-245”的同学的Cno、Sno和Degree,并按Degree从高到低次序排序', () => {
+        fail('unimplement');
     });
 
-    it("查询选修编号为“3-105”且成绩高于选修编号为“3-245”课程的同学的Cno、Sno和Degree.", () => {
-        fail("unimplement");
+    it('查询选修编号为“3-105”且成绩高于选修编号为“3-245”课程的同学的Cno、Sno和Degree.', () => {
+        fail('unimplement');
     });
 
-    it("查询所有教师和同学的name、sex和birthday.", () => {
-        fail("unimplement");
+    it('查询所有教师和同学的name、sex和birthday.', () => {
+        fail('unimplement');
     });
 
-    it("查询所有“女”教师和“女”同学的name、sex和birthday.", () => {
-        fail("unimplement");
+    it('查询所有“女”教师和“女”同学的name、sex和birthday.', () => {
+        fail('unimplement');
     });
 
-    it("查询成绩比该课程平均成绩低的同学的成绩", () => {
-        fail("unimplement");
+    it('查询成绩比该课程平均成绩低的同学的成绩', () => {
+        fail('unimplement');
     });
 
-    it("查询所有任课教师的Tname和Depart.", () => {
-        fail("unimplement");
+    it('查询所有任课教师的Tname和Depart.', () => {
+        fail('unimplement');
     });
 
-    it("查询所有未讲课的教师的Tname和Depart.", () => {
-        fail("unimplement");
+    it('查询所有未讲课的教师的Tname和Depart.', () => {
+        fail('unimplement');
     });
 
-    it("查询至少有2名男生的班号", () => {
-        fail("unimplement");
+    it('查询至少有2名男生的班号', () => {
+        fail('unimplement');
     });
 
-    it("查询Student中不姓“王”的同学记录", () => {
-        fail("unimplement");
+    it('查询Student中不姓“王”的同学记录', () => {
+        fail('unimplement');
     });
 
-    it("查询Student中每个学生的姓名和年龄", () => {
-        fail("unimplement");
+    it('查询Student中每个学生的姓名和年龄', () => {
+        fail('unimplement');
     });
 
-    it("查询Student中最大和最小的Sbirthday日期值", () => {
-        fail("unimplement");
+    it('查询Student中最大和最小的Sbirthday日期值', () => {
+        fail('unimplement');
     });
 
-    it("以班号和年龄从大到小的顺序查询Student中的全部记录", () => {
-        fail("unimplement");
+    it('以班号和年龄从大到小的顺序查询Student中的全部记录', () => {
+        fail('unimplement');
     });
 
-    it("查询“男”教师及其所上的课程", () => {
-        fail("unimplement");
+    it('查询“男”教师及其所上的课程', () => {
+        fail('unimplement');
     });
 
-    it("查询最高分同学的Sno、Cno和Degree列", () => {
-        fail("unimplement");
+    it('查询最高分同学的Sno、Cno和Degree列', () => {
+        fail('unimplement');
     });
 
-    it("查询和“李军”同性别的所有同学的Sname.", () => {
-        fail("unimplement");
+    it('查询和“李军”同性别的所有同学的Sname.', () => {
+        fail('unimplement');
     });
 
-    it("查询和“李军”同性别并同班的同学Sname.", () => {
-        fail("unimplement");
+    it('查询和“李军”同性别并同班的同学Sname.', () => {
+        fail('unimplement');
     });
 
-    it("查询所有选修“计算机导论”课程的“男”同学的成绩", () => {
-        fail("unimplement");
+    it('查询所有选修“计算机导论”课程的“男”同学的成绩', () => {
+        fail('unimplement');
     });
 
 });
